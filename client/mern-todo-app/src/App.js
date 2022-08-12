38df0348b10099ef5c23bb48435ec746a533f1be
Muhammad Alyan <alyan0332@gmail.com>
2022-08-12 20:38:12 +0500

    change component name

--- client/mern-todo-app/src/App.js
+++ client/mern-todo-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world!</h1>
+      <RegisterPage />
     </div>
   );
 }
