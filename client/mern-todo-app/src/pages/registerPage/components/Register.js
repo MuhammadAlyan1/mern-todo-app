9b924f736ad60d6520f138af7b6ae34646c26eeb
Muhammad Alyan <alyan0332@gmail.com>
2022-08-14 00:15:34 +0500

    set condition to show snackbar

--- client/mern-todo-app/src/pages/registerPage/components/Register.js
+++ client/mern-todo-app/src/pages/registerPage/components/Register.js
@@ -3,60 +3,64 @@ import { Box } from '@mui/system';
 import React, { useState, useContext } from 'react';
 import { todoAppContext } from '../../../App';
 import registerUser from '../../../api/registerUser';
+import ShowSnackbar from '../../../components/ShowSnackbar';
 
 const Register = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const { dispatch } = useContext(todoAppContext);
+  const { state, dispatch } = useContext(todoAppContext);
 
   return (
-    <Grid
-      container
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: '90vh' }}
-    >
-      <Stack
-        spacing={1.5}
-        sx={{
-          minWidth: {
-            xs: '300px',
-            sm: '400px',
-          },
-        }}
+    <>
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '90vh' }}
       >
-        <Typography textAlign="center" variant="h4" component="div">
-          Register
-        </Typography>
-        <TextField
-          onChange={(e) => setUsername(e.target.value)}
-          value={username}
-          label="Username"
-          variant="outlined"
-          size="small"
-        />
-        <TextField
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-          label="Password"
-          type="password"
-          variant="outlined"
-          size="small"
-        />
-        <Button
-          variant="contained"
-          onClick={() => registerUser({ username, password }, dispatch)}
+        <Stack
+          spacing={1.5}
+          sx={{
+            minWidth: {
+              xs: '300px',
+              sm: '400px',
+            },
+          }}
         >
-          Register
-        </Button>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography>Already have an account?</Typography>
-          <Button variant="text" color="error">
-            Login
+          <Typography textAlign="center" variant="h4" component="div">
+            Register
+          </Typography>
+          <TextField
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            label="Username"
+            variant="outlined"
+            size="small"
+          />
+          <TextField
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            label="Password"
+            type="password"
+            variant="outlined"
+            size="small"
+          />
+          <Button
+            variant="contained"
+            onClick={() => registerUser({ username, password }, dispatch)}
+          >
+            Register
           </Button>
-        </Box>
-      </Stack>
-    </Grid>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography>Already have an account?</Typography>
+            <Button variant="text" color="error">
+              Login
+            </Button>
+          </Box>
+        </Stack>
+      </Grid>
+      {state.isSnackbarShowing && <ShowSnackbar />}
+    </>
   );
 };
 
