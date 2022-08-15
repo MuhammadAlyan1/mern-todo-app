0081de8b036f73e71be899a4bbbd5837bcc0dee6
Muhammad Alyan <alyan0332@gmail.com>
2022-08-15 23:06:10 +0500

    add cases for todos

--- client/mern-todo-app/src/reducer.js
+++ client/mern-todo-app/src/reducer.js
@@ -18,6 +18,49 @@ const reducer = (state, action) => {
       };
     }
 
+    case 'SET_TODOS': {
+      const { todos } = payload;
+      return {
+        ...state,
+        listOfTodos: [...todos],
+      };
+    }
+
+    case 'ADD_NEW_TODO': {
+      const { newTodo } = payload;
+      return {
+        ...state,
+        listOfTodos: [...state.listOfTodos, newTodo],
+      };
+    }
+
+    case 'UPDATE_TODO': {
+      const { updatedTodo } = payload;
+      const { _id } = updatedTodo;
+      const newListOfTodos = state.listOfTodos.map((todo) => {
+        if (_id === todo._id) {
+          return updatedTodo;
+        }
+        return todo;
+      });
+
+      return {
+        ...state,
+        listOfTodos: [...newListOfTodos],
+      };
+    }
+
+    case 'DELETE_TODO': {
+      const { todoId } = payload;
+      const newTodoList = state.listOfTodos.filter(
+        (todo) => todo._id !== todoId
+      );
+      return {
+        ...state,
+        listOfTodos: [...newTodoList],
+      };
+    }
+
     case 'SET_SNACKBAR': {
       const { isSnackbarShowing, snackbarMessage, snackbarSeverity } = payload;
       return {
