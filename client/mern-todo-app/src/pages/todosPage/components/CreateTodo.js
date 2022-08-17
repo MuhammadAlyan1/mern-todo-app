055606da574dd054ea6ee4ce400521e015126993
Muhammad Alyan <alyan0332@gmail.com>
2022-08-17 14:20:56 +0500

    split code into different components

--- client/mern-todo-app/src/pages/todosPage/components/CreateTodo.js
+++ client/mern-todo-app/src/pages/todosPage/components/CreateTodo.js
@@ -1,84 +1,28 @@
 import React, { useState, useContext } from 'react';
-import { Box } from '@mui/system';
-import { IconButton, Modal, Stack, TextField } from '@mui/material';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import { Fab, Tooltip } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import createTodo from '../../../api/createTodo';
 import { todoAppContext } from '../../../App';
 import ShowSnackbar from '../../../components/ShowSnackbar';
+import CreateTodoFloatingButton from './CreateTodoFloatingButton';
+import CreateTodoModal from './CreateTodoModal';
 
 const CreateTodo = () => {
-  const { state, dispatch } = useContext(todoAppContext);
+  const {
+    state: { isSnackbarShowing },
+  } = useContext(todoAppContext);
   const [isTodoModalShowing, setIsTodoModalShowing] = useState(false);
-  const [todoText, setTodoText] = useState('');
-  const { userId } = state;
-
-  const handleCreateTodo = () => {
-    if (todoText === '') {
-      dispatch({
-        type: 'SET_SNACKBAR',
-        payload: {
-          isSnackbarShowing: true,
-          snackbarMessage: 'Todo contents cannot be empty',
-          snackbarSeverity: 'warning',
-        },
-      });
-      return;
-    }
-    createTodo({ userId, todoText, dispatch });
-    setTodoText('');
-  };
 
   return (
     <>
-      <Tooltip title="Add New Task" placement="top">
-        <Fab
-          color="primary"
-          sx={{
-            position: 'fixed',
-            bottom: 30,
-            right: 30,
-          }}
-          onClick={() => setIsTodoModalShowing(!isTodoModalShowing)}
-        >
-          <AddIcon />
-        </Fab>
-      </Tooltip>
+      <CreateTodoFloatingButton
+        isTodoModalShowing={isTodoModalShowing}
+        setIsTodoModalShowing={setIsTodoModalShowing}
+      />
 
-      <Modal
-        open={isTodoModalShowing}
-        onClose={() => setIsTodoModalShowing(false)}
-      >
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '20%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            padding: 2,
-            backgroundColor: '#333',
-            width: { xs: '250px', sm: '500px' },
-            height: '100px',
-            borderRadius: 2,
-          }}
-        >
-          <Stack direction="row" alignItems="center" mt="25px">
-            <TextField
-              label="Add New Task"
-              size="small"
-              variant="outlined"
-              fullWidth
-              value={todoText}
-              onChange={(e) => setTodoText(e.target.value)}
-            />
-            <IconButton shape="square" onClick={handleCreateTodo}>
-              <AddBoxIcon fontSize="large" color="primary" />
-            </IconButton>
-          </Stack>
-        </Box>
-      </Modal>
-      {state.isSnackbarShowing && <ShowSnackbar />}
+      <CreateTodoModal
+        isTodoModalShowing={isTodoModalShowing}
+        setIsTodoModalShowing={setIsTodoModalShowing}
+      />
+
+      {isSnackbarShowing && <ShowSnackbar />}
     </>
   );
 };
