ef1de10a519c236c475768341257445cfd698692
Muhammad Alyan <alyan0332@gmail.com>
2022-08-17 14:19:43 +0500

    seperate sx prop

--- client/mern-todo-app/src/pages/loginPage/components/Login.js
+++ client/mern-todo-app/src/pages/loginPage/components/Login.js
@@ -12,6 +12,27 @@ const Login = () => {
 
   const { state, dispatch } = useContext(todoAppContext);
 
+  const styles = {
+    stack: {
+      color: '#eee',
+      gap: 1.5,
+      minWidth: {
+        xs: '300px',
+        sm: '400px',
+      },
+    },
+
+    box: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+
+    button: {
+      color: 'error.main',
+      '&:hover': { backgroundColor: 'transparent' },
+    },
+  };
+
   return (
     <>
       {state.userId && <Navigate to="/todosPage" />}
@@ -21,29 +42,20 @@ const Login = () => {
         justifyContent="center"
         style={{ minHeight: '90vh' }}
       >
-        <Stack
-          spacing={1.5}
-          sx={{
-            minWidth: {
-              xs: '300px',
-              sm: '400px',
-            },
-            color: '#eee',
-          }}
-        >
+        <Stack sx={styles.stack}>
           <Typography textAlign="center" variant="h4" component="div">
             Login
           </Typography>
           <TextField
-            onChange={(e) => setUsername(e.target.value)}
             value={username}
+            onChange={(e) => setUsername(e.target.value)}
             label="Username"
             variant="outlined"
             size="small"
           />
           <TextField
-            onChange={(e) => setPassword(e.target.value)}
             value={password}
+            onChange={(e) => setPassword(e.target.value)}
             label="Password"
             type="password"
             variant="outlined"
@@ -55,14 +67,10 @@ const Login = () => {
           >
             Login
           </Button>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={styles.box}>
             <Typography>Don't have an account yet?</Typography>
             <Link to="/register" style={{ textDecoration: 'none' }}>
-              <Button
-                variant="text"
-                color="error"
-                sx={{ '&:hover': { backgroundColor: 'transparent' } }}
-              >
+              <Button variant="text" sx={styles.button}>
                 Register
               </Button>
             </Link>
